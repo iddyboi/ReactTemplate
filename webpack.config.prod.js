e9fa293f308ed8b4a963d017258f3398c7b5b7bb
Idris <idrist2013@gmail.com>
2020-07-07 19:32:13 +0100

    Boiler plater for React setup using webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const htmlwebpackplugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "",
+  },
+  devtool: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              webpackImporter: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=8000&name=images/[name].[ext]",
+      },
+    ],
+  },
+  plugins: [
+    new htmlwebpackplugin({
+      template: __dirname + "/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
